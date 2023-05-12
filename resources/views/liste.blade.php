cda7c3a1feb5f9d51b0354f15b0f4d143339673d
MendrikaRaj <99309350+MendrikaRaj@users.noreply.github.com>
2023-05-12 21:16:28 +0300

    Design

--- resources/views/liste.blade.php
+++ resources/views/liste.blade.php
@@ -19,7 +19,7 @@
 
 <body>
     <nav class="navbar navbar-light navbar-expand-lg fixed-top" id="mainNav">
-        <div class="container"><a class="navbar-brand" href="liste.html">Ai News</a><button data-bs-toggle="collapse" data-bs-target="#navbarResponsive" class="navbar-toggler" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+        <div class="container"><a class="navbar-brand" href="#"><span><img src="assets/img/computer.png" alt="Icon"></span>IA Trends</a><button data-bs-toggle="collapse" data-bs-target="#navbarResponsive" class="navbar-toggler" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto"></ul>
             </div>
