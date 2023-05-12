2599ab27e2e4e506f7db650699b1e1e19b2ad6ae
MendrikaRaj <99309350+MendrikaRaj@users.noreply.github.com>
2023-05-12 21:21:21 +0300

    design

--- resources/views/poste.blade.php
+++ resources/views/poste.blade.php
@@ -18,7 +18,7 @@
 
 <body>
     <nav class="navbar navbar-light navbar-expand-lg fixed-top" id="mainNav">
-        <div class="container"><a class="navbar-brand" href="#"><span><img src="assets/img/computer.png" alt="Icon"></span>IA Trends</a><button data-bs-toggle="collapse" data-bs-target="#navbarResponsive" class="navbar-toggler" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+        <div class="container"><a class="navbar-brand" href="#"><span><img src="assets/img/computer.png" alt="Icon" style="font-size: 1.2em"></span>IA Trends</a><button data-bs-toggle="collapse" data-bs-target="#navbarResponsive" class="navbar-toggler" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item"><a class="nav-link" href="logout">Log out</a></li>
